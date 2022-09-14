2a39445ed058273aef9c444393b2d5bcc0c0f8fe
hedgeera <102842146+hedgeera@users.noreply.github.com>
2022-09-14 18:57:07 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,6 @@ module.exports = {
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-sharp",
     "gatsby-plugin-image",
-    "gatsby-plugin-theme-ui",
     {
       resolve: "gatsby-source-contentful",
       options: {
