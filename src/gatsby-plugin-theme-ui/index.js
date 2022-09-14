f8820927bc6a96027c5da78a5d5af35a6e2f21ab
hedgeera <102842146+hedgeera@users.noreply.github.com>
2022-09-14 18:46:20 -0500

    Update index.js

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,4 +1,5 @@
 export default {
+  initialColorModeName: 'light',
   fonts: {
     body: 'system-ui, sans-serif',
     heading: '"Avenir Next", sans-serif',
@@ -8,5 +9,22 @@ export default {
     text: '#000',
     background: '#fff',
     primary: '#33e',
+    modes: {
+      dark: {
+        text: '#fff',
+        background: '#000',
+        primary: '#cc1'
+      },
+      tomato: {
+        text: '#565656',
+        background: 'tomato',
+        primary: 'tomato',
+      },
+      deep: {
+        text: 'hsl(210, 50%, 96%)',
+        background: 'hsl(230, 25%, 18%)',
+        primary: 'hsl(260, 100%, 80%)',
+      },
+    }
   },
 }
